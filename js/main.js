d8c5d03593ac3ed1e8861cbfbb505a63e62e9f99
Nikita <temnigor@gmail.com>
2022-03-10 18:38:54 +0400

    Отрисуй меня полностью (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,1 @@
-import { getRandomJsObject } from './infoHotel.js';
-const NUMBER_OBJEKT_JS = 10;
-const arrayTenRandomHjtelObject = Array.from({length:NUMBER_OBJEKT_JS}, getRandomJsObject);
-
+import './adsGeneration.js';
