883d55611da8665e0a66609d820ec62ccf0c5d45
Nikita <temnigor@gmail.com>
2022-03-31 20:01:18 +0400

    Исправления lint

--- js/api.js
+++ js/api.js
@@ -1,7 +1,6 @@
 import {onRemoveMassage} from'./formValidation.js';
-const getData = (onSuccess, onError)=>{
-  return fetch(
-'https://25.javascript.pages.academy/keksobooking/data',
+const getData = (onSuccess, onError)=>fetch(
+  'https://25.javascript.pages.academy/keksobooking/data',
   {
     method:'GET',
     credentials: 'same-origin',
@@ -9,41 +8,38 @@ const getData = (onSuccess, onError)=>{
 )
   .then((response)=>{
     if(response.ok){
-    return response.json()
+      return response.json();
     }
-    throw new Error(`${response.status} ${response.statusText}`)
+    throw new Error(`${response.status} ${response.statusText}`);
   }
-    )
-  .then((data)=>{
-   return onSuccess(data)
-  })
+  )
+  .then((data)=>onSuccess(data))
   .catch((err)=>{
-    onError(err)
-})
-}
+    onError(err);
+  });
 
 const getSubmit = (onSaccessSubmit, onErrorSubmite, form, buttonSubmit,pristine)=>{
-form.addEventListener('submit', (evt)=>{
-  evt.preventDefault();
-  buttonSubmit.disabled=true;
-  const isValid = pristine.validate();
-if (isValid){
-  const formData = new FormData(evt.target);
-  fetch(
-    'https://25.javascript.pages.academy/keksobooking',
-  {
-    method:'POST',
-   body: formData,
-  }
-)
-  .then(()=>{
-    onSaccessSubmit(onRemoveMassage)
-  })
-  .catch((err)=>{
-   onErrorSubmite(err,onRemoveMassage)
+  form.addEventListener('submit', (evt)=>{
+    evt.preventDefault();
+    buttonSubmit.disabled=true;
+    const isValid = pristine.validate();
+    if (isValid){
+      const formData = new FormData(evt.target);
+      fetch(
+        'https://25.javascript.pages.academy/keksobooking',
+        {
+          method:'POST',
+          body: formData,
+        }
+      )
+        .then(()=>{
+          onSaccessSubmit(onRemoveMassage);
+        })
+        .catch((err)=>{
+          onErrorSubmite(err,onRemoveMassage);
 
+        });
+    }
   });
-}
-});
 };
-export{getData, getSubmit}
+export{getData, getSubmit};
